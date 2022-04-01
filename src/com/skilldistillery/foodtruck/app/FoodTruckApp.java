ba66a2d28b7d1eba69c02d1893648e1ee7da2017
Chris Brite <chrismbrite@gmail.com>
2022-04-01 03:50:45 -0700

    Menu first option working without null

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -7,17 +7,16 @@ import com.skilldistillery.foodtruck.entities.FoodTruck;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
+		FoodTruckApp app = new FoodTruckApp();
+		app.run();
+	}
+
+	FoodTruck[] trucks = new FoodTruck[5];
+
+	public void run() {
 		Scanner sc = new Scanner(System.in);
-
-		FoodTruck[] trucks = new FoodTruck[5];
-		trucks[0] = new FoodTruck();
-		trucks[1] = new FoodTruck();
-		trucks[2] = new FoodTruck();
-		trucks[3] = new FoodTruck();
-		trucks[4] = new FoodTruck();
-
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Please enter the food truck name: ");
+		for (int i = 0; i < trucks.length; i++) {
+			System.out.println("Please enter the food truck name or type Quit to view menu. ");
 			String truckName = sc.nextLine();
 			int nextTruckId = 0;
 			nextTruckId = i + 1;
@@ -27,43 +26,47 @@ public class FoodTruckApp {
 			System.out.println("Please enter the food type: ");
 			String foodType = sc.nextLine();
 
-			System.out.println("Please rate the food truck: ");
+			System.out.println("Please rate the food truck 1-10: ");
 			double truckRating = sc.nextDouble();
 			sc.nextLine();
 
 			FoodTruck copyTrucks = new FoodTruck(nextTruckId, truckName, foodType, truckRating);
 			trucks[i] = copyTrucks;
-			System.out.println(copyTrucks);
-			System.out.println();
+
+//			System.out.println(trucks[i].toString());
+//			System.out.println();
+
 		}
+		menu();
+		int choice = sc.nextInt();
+
+		if (choice == 1) {
+			for (int i = 0; i < trucks.length; i++) {
+				if (trucks[i] != null) {
+					System.out.println(trucks[i]);
+				}
+			}
+		} else if (choice == 2) {
+//			trucks[i].getTruckRating();
+
+//		else if(choice == 3) {
+//		System.out.print(bestTruck);
+		} else if (choice == 4) {
+			System.out.print("GoodBye");
+//			break;
+		} else {
+			System.out.println("Invalid menu choice");
+//			break;
+		}
+		sc.close();
+	}
+
+	public void menu() {
 
 		System.out.println("Please select a number for the corresponding menu choice.");
 		System.out.println("1. List all existing food trucks.");
 		System.out.println("2. See the average rating of food trucks.");
 		System.out.println("3. Display the highest-rated food truck.");
 		System.out.println("4. Quit the program.");
-		int choice = sc.nextInt();
-
-		for (int i = 0; i < trucks.length; i++) {
-			if (choice == 1) {
-				System.out.println(trucks.toString());
-			} else {
-				System.out.println("Invalid menu choice");
-				break;
-			}
-			
-//		else if(choice == 2) {
-//		double average = truckRating / numTrucks;
-//		System.out.print(average);
-//		}
-//		else if(choice == 3) {
-//		System.out.print(bestTruck);
-//		}
-//			else if(choice == 3) {
-//				System.out.print(bestTruck);
-//				}
-
-			sc.close();
-		}
 	}
 }
